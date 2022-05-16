650dadc8645f2f27253ccefad5e833cf702f4bc3
Julio Henrique de Oliveira <julioholiveira7@gmail.com>
2022-05-15 21:57:19 -0300

    Create aula006.py
    Código exemplo da aula 6 de Python do Curso em Video.

---
+++ CursoEmVideo/aula006.py
@@ -0,0 +1,17 @@
+#método 1
+n1 = input('Digite o primeiro valor: ');
+n2 = input('Digite o segundo valor: ');
+soma = int(n1) + int(n2);
+print('A soma do primeiro método é: ', soma);
+
+#método 2
+n3 = int(input('Digite o primeiro valor: '));
+n4 = int(input('Digite o segundo valor: '));
+soma2 = n3 + n4;
+print('A soma do segundo método é: ', soma2);
+
+#método 3
+n5 = int(input('Digite o primeiro valor: '));
+n6 = int(input('Digite o segundo valor: '));
+soma3 = n5 + n6;
+print('A soma entre {} e {} é: {}'.format(n5, n6, soma3));
