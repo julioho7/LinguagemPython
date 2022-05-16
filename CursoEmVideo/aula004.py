233bef07b02e501dbaccf18a7e6de3f820f0bd1e
Julio Henrique de Oliveira <julioholiveira7@gmail.com>
2022-05-15 21:52:58 -0300

    Create aula004.py
    Exercício feito na aula 004 do curso em Python.

---
+++ CursoEmVideo/aula004.py
@@ -0,0 +1,5 @@
+nome = input('Qual é o seu nome? ');
+sobrenome = input('Qual é o seu sobrenome? ');
+idade = input('Qual é sua idade? ');
+peso = input('Qual é o seu peso? ');
+print('Olá, ' + nome + ' ' + sobrenome + '. Você possui  ' + idade + ' anos de idade e seu peso é ' + peso + 'Kg');
